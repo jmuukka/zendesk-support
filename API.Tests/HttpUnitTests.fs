eacfa4cd45e6aa5941708ecbb1c5b11f52a77188
BLACK\Jarmo <jarmo.muukka@gmail.com>
2020-01-21 22:58:27 +0200

    Improved unit tests of getArray

--- API.Tests/HttpUnitTests.fs
+++ API.Tests/HttpUnitTests.fs
@@ -7,6 +7,15 @@ open System.Net.Http
 open Xunit
 open Mutex.Zendesk.Support.API
 
+let context =
+    {
+        BaseUrl = Uri("https://localhost/", UriKind.Absolute)
+        Credentials = UsernameToken {
+            Username = "will.be@igno.red"
+            Token = "it's a secret :)"
+        }
+    }
+
 type Item = {
     id : int
 }
@@ -32,44 +41,47 @@ let createQueue uris =
     uris |> List.iter enqueue
     queue
 
-let context =
-    {
-        BaseUrl = Uri("https://localhost/", UriKind.Absolute)
-        Credentials = UsernameToken {
-            Username = "will.be@igno.red"
-            Token = "it's a secret :)"
-        }
-    }
+let createResponse (queue : Queue<ItemsModel>) =
+    let items = queue.Dequeue()
+    let content = Json.serialize items
+    let response = new HttpResponseMessage(HttpStatusCode.OK)
+    response.Content <- new StringContent(content, System.Text.Encoding.UTF8, "application/json")
+    response
 
-// This has too many responsibilities. Need to split it.
-[<Fact>]
-let ``getArray with many pages then all pages received`` () =
+let ``arrange for getArray`` () =
     let firstPage = "http://api/"
     let nextPages = [firstPage + "?page=2"; firstPage + "?page=3"; null]
-    let expectedPages = firstPage :: List.take 2 nextPages
     let queue = createQueue nextPages
-    let mutable uris = []
-
+    let uris = List<string>()
     let send : HttpSend =
         fun ctx createReq ->
             let request = createReq ctx
-            uris <- uris @ [request.RequestUri.AbsoluteUri]
-            let items = queue.Dequeue()
-            let content = Json.serialize items
-            let response = new HttpResponseMessage(HttpStatusCode.OK)
-            response.Content <- new StringContent(content, System.Text.Encoding.UTF8, "application/json")
-            Ok response
+            uris.Add(request.RequestUri.AbsoluteUri)
+            createResponse queue
+            |> Ok
+    firstPage, nextPages, queue, uris, send
+
+[<Fact>]
+let ``getArray with many pages then data of all pages received`` () =
+    let firstPage, _, _, _, send = ``arrange for getArray``()
 
     let actual = Http.getArray<ItemsModel, Item> send context (fun model -> model.items) firstPage
 
-    Assert.equals 0 queue.Count
-    Assert.equals expectedPages uris
-    Assert.ok actual
     match actual with
     | Ok items ->
         Assert.equals [|{id=1}; {id=2}; {id=3}|] items
-        ()
-    | _ -> ()
+    | _ ->
+        failwith "Error"
+
+[<Fact>]
+let ``getArray with many pages then pages consumed expected pages requested`` () =
+    let firstPage, nextPages, queue, uris, send = ``arrange for getArray``()
+
+    Http.getArray<ItemsModel, Item> send context (fun model -> model.items) firstPage
+    |> ignore
+
+    Assert.equals 0 queue.Count
+    Assert.equals (firstPage :: List.take 2 nextPages) (uris |> List.ofSeq)
 
 //[<Fact>]
 //let ``send`` () =
